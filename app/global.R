82994aa6754702f98f32745bf2c492e471215afd
ishitapatel2002 <158802021+ishitapatel2002@users.noreply.github.com>
2024-05-03 23:49:45 -0400

    Update global.R

--- app/global.R
+++ app/global.R
@@ -1,11 +1,15 @@
 library(httr)
 library(jsonlite)
+library(tm)
+library(dplyr)
+library(tidytext)
+library(syuzhet)
 
 subreddits_women <- c("Feminism", "TwoXChromosomes", "AskWomen")
 subreddits_men <- c("TheRedPill", "MensRights", "AskMen", "MensLib")
 subreddits_neutral <- c("news", "worldnews", "AskReddit")
 
-fetch_reddit_data <- function(subreddit_vector, terms, limit = 100, sort = "desc", sort_type = "created_utc") {
+fetch_reddit_data <- function(subreddit_vector, terms, limit = 500, sort = "desc", sort_type = "created_utc") {
   all_posts <- list()
   
   for (subreddit in subreddit_vector) {
@@ -44,5 +48,51 @@ saveRDS(posts_neutral, file = "posts_neutral.rds")
 posts_women <- readRDS("posts_women.rds")
 posts_men <- readRDS("posts_men.rds")
 posts_neutral <- readRDS("posts_neutral.rds")
+
+create_word_freq_df <- function(posts) {
+  words <- tolower(unlist(strsplit(posts$title, "\\s+")))
+  words <- removePunctuation(words)
+  words <- words[!words %in% stopwords("en")]
+  words <- words[words != "" & !grepl("^[0-9]+$", words)]
+  
+  word_freq <- table(words)
+  word_freq_df <- as.data.frame(word_freq, stringsAsFactors = FALSE)
+  word_freq_df <- word_freq_df %>% arrange(desc(Freq)) %>% rename(word = words, freq = Freq)
+  return(word_freq_df)
+}
+
+#save word frequencies as rds files
+save_word_freq <- function(subreddit_data, filename) {
+  word_freq_df <- create_word_freq_df(subreddit_data)
+  saveRDS(word_freq_df, filename)
+}
+
+# save for all subreddits
+save_word_freq(posts_women[["Feminism"]], "feminism_freq.rds")
+save_word_freq(posts_women[["TwoXChromosomes"]], "twoxchromosomes_freq.rds")
+save_word_freq(posts_women[["AskWomen"]], "askwomen_freq.rds")
+save_word_freq(posts_men[["TheRedPill"]], "theredpill_freq.rds")
+save_word_freq(posts_men[["MensRights"]], "mensrights_freq.rds")
+save_word_freq(posts_men[["AskMen"]], "askmen_freq.rds")
+save_word_freq(posts_men[["MensLib"]], "menslib_freq.rds")
+save_word_freq(posts_neutral[["news"]], "news_freq.rds")
+save_word_freq(posts_neutral[["worldnews"]], "worldnews_freq.rds")
+save_word_freq(posts_neutral[["AskReddit"]], "askreddit_freq.rds")
+
+
+# for easy access in server.R
+precomputed_freqs <- list(
+  "Feminism" = readRDS("feminism_freq.rds"),
+  "TwoXChromosomes" = readRDS("twoxchromosomes_freq.rds"),
+  "AskWomen" = readRDS("askwomen_freq.rds"),
+  "TheRedPill" = readRDS("theredpill_freq.rds"),
+  "MensRights" = readRDS("mensrights_freq.rds"),
+  "AskMen" = readRDS("askmen_freq.rds"),
+  "MensLib" = readRDS("menslib_freq.rds"),
+  "news" = readRDS("news_freq.rds"),
+  "worldnews" = readRDS("worldnews_freq.rds"),
+  "AskReddit" = readRDS("askreddit_freq.rds")
+)
+
 
 
